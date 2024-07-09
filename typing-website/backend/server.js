b5c4150b97b5bae180e2d7c1d891b887365f21b3
aaryadixit21 <dixitaarya3@gmail.com>
2024-07-09 13:09:57 +0530

    fixed DB connnection issue

--- typing-website/backend/server.js
+++ typing-website/backend/server.js
@@ -1,20 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
-import connectDB from './backendconfig.js';
 
-dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-connectDB();
-
-mongoose.connect(process.env.MONGO_URI, {
+const MONGO_URI="mongodb+srv://aarya20225001:Fvb6TpKKfzAWVyNJ@cluster0.jr9dr2g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
@@ -23,5 +20,5 @@ mongoose.connect(process.env.MONGO_URI, {
 
 app.use('/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
